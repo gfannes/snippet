cc39d83a8005450f375e42f6b97a28c732243fe0
Geert Fannes <geert.fannes@gmail.com>
2025-02-19 07:52:12 +0100

    wip

--- dirwalk.zig
+++ dirwalk.zig
@@ -1,12 +1,24 @@
 const std = @import("std");
 const ut = std.testing;
 
-test "ut" {
+test "iterate" {
+    // var dir = try std.fs.cwd().openDir(".", .{ .iterate = true });
+    // defer dir.close();
+
+    // var it = dir.iterate();
+    // while (try it.next()) |el| {
+    //     std.debug.print("{s}\n", .{el.name});
+    // }
+}
+
+test "walk" {
     var dir = try std.fs.cwd().openDir(".", .{ .iterate = true });
     defer dir.close();
 
-    var it = dir.iterate();
-    while (try it.next()) |el| {
-        std.debug.print("{s}\n", .{el.name});
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const ma = gpa.allocator();
+    var walker = try dir.walk(ma);
+    while (try walker.next()) |el| {
+        std.debug.print("{s} {s} {}\n", .{ el.basename, el.path, el.kind });
     }
 }
