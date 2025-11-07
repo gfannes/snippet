def6c40775eb48d9355bfa57cd4dfdc882a4cd9b
Geert Fannes <geert.fannes@gmail.com>
2025-11-07 09:58:53 +0100

    wip

--- array.zig
+++ array.zig
@@ -1,9 +1,18 @@
 const std = @import("std");
 
+fn arrayLenOf(T: type) usize {
+    return switch (@typeInfo(T)) {
+        .array => |a| a.len,
+        else => @compileError("Expected T to be an array"),
+    };
+}
+
 test "array" {
     const ut = std.testing;
 
     const Array = [10]u8;
+
+    try ut.expectEqual(10, arrayLenOf(Array));
 
     const array: Array = undefined;
     try ut.expectEqual(10, array.len);
