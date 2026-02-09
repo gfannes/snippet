a3083b2ec8b5058ec2dcb4c1537ced30b033a0bc
Geert Fannes <geert.fannes@gmail.com>
2026-02-09 21:24:10 +0100

    fix

--- process.zig
+++ process.zig
@@ -1,36 +1,33 @@
 const std = @import("std");
+const ut = std.testing;
 
-fn read(file: std.fs.File, caption: []const u8) !void {
-    var buf: [16]u8 = undefined;
-    while (true) {
-        const n = try file.readAll(&buf);
-        if (n == 0)
-            break;
-        std.debug.print("{s}:{}=>{s}\n", .{ caption, n, buf[0..n] });
-    }
+fn read(file: std.Io.File, caption: []const u8) !void {
+    var rbuf: [100]u8 = undefined;
+    var reader = file.reader(ut.io, &rbuf);
+
+    while (reader.interface.peekGreedy(1)) |str| {
+        defer reader.interface.tossBuffered();
+        std.debug.print("{s}:{}=>{s}\n", .{ caption, str.len, str });
+    } else |_| {}
 }
 
 test "process" {
-    const ut = std.testing;
-
     const cmd = [_][]const u8{"rake"};
-
-    var proc = std.process.Child.init(&cmd, ut.allocator);
-    proc.stdout_behavior = .Pipe;
-    proc.stderr_behavior = .Ignore;
-
     const dir_fp = "/home/geertf/am";
-    var dir = try std.fs.openDirAbsolute(dir_fp, .{});
-    defer dir.close();
-    proc.cwd_dir = dir;
 
-    try proc.spawn();
+    var proc = try std.process.spawn(ut.io, .{
+        .argv = &cmd,
+        .cwd = dir_fp,
+        .stdout = .pipe,
+        .stderr = .pipe,
+    });
+    defer proc.kill(ut.io);
 
     if (proc.stdout) |stdout|
         try read(stdout, "stdout");
     if (proc.stderr) |stderr|
         try read(stderr, "stderr");
 
-    const term = try proc.wait();
+    const term = try proc.wait(ut.io);
     std.debug.print("term: {}\n", .{term});
 }
